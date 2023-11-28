293f525b6834853b60f9bd4c98bb907457ff4aad
Lio95 <lio.francois95@gmail.com>
2023-11-28 16:06:36 +0100

    Modif

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'home.html'));
+	res.sendFile(path.join(__dirname, 'public', 'CV.pdf'));
 });
 
 app.listen(8000, () => {
-	console.log("Server successfully running on port 8000");
+	console.log("Server successfully running on port 8080");
 });
